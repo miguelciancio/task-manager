b3b5e8934373a408549aff4a87e2ce2ce10a58b4
miguelciancio <mciancio.figueiredo@gmail.com>
2022-12-25 19:59:47 +0000

    Menu Update - Refactored Menu Section Code

--- task_manager.py
+++ task_manager.py
@@ -32,6 +32,13 @@ is_invalid = False
 is_password = False
 is_username = False
 
+# open tasks.txt file in order to read and store its contents to the user on future operations in another variable (task_list)
+with open("tasks.txt", "r", encoding="utf-8") as file:
+    # iterate the file
+    for line in file:
+        line = line.strip(os.linesep).split(", ") # first, get each line of the file and then create an individual list for each task's information, excluding the \n escape characters at the end of the word
+        task_list.append(line) # append the tasks information list into another variable (task_list)
+
 
 # ===== LOGIN SECTION =====
 '''Here we have a code that will allow a user to login.
@@ -193,16 +200,6 @@ e  \t- \tExit
             - Split the line where there is comma and space.
             - Then print the results
         '''
-        # clear task_list in order to always display the current tasks on the system
-        # otherwise, it will append the same tasks in the list and display them to
-        # the user
-        task_list = []  
-        # open tasks.txt file in order to read and display its contents to the user
-        with open("tasks.txt", "r", encoding="utf-8") as file:
-            # iterate the file
-            for line in file:
-                line = line.strip(os.linesep).split(", ") # first, get each line of the file and then create an individual list for each task's information, excluding the \n escape characters at the end of the word
-                task_list.append(line) # append the tasks information list into another
         time.sleep(0.7)
         # for loop that iterates through task_list in order to
         # extract each value and print out to the user
@@ -222,16 +219,6 @@ e  \t- \tExit
             read from the file.
             - If they are the same print it out
         '''
-        # clear task_list in order to always display the current tasks on the system
-        # otherwise, it will append the same tasks in the list and display them to
-        # the user
-        task_list = []
-        # open tasks.txt file in order to read and display its contents to the user
-        with open("tasks.txt", "r", encoding="utf-8") as file:
-            # iterate the file
-            for line in file:
-                line = line.strip(os.linesep).split(", ") # first, get each line of the file and then create an individual list for each task's information, excluding the \n escape characters at the end of the word
-                task_list.append(line) # append the tasks information list into another
         time.sleep(0.7)
         # for loop that iterates through task_list in order to
         # extract each value and print out to the user
@@ -239,9 +226,7 @@ e  \t- \tExit
             if username_input == values[0]:
                 print(f"""\nTask: \t\t\t{values[0]} \nAssigned to: \t\t{values[1]} \nDate assigned: \t\t{values[3]} \nDue date: \t\t{values[4]} \nTask complete? \t\t{values[5]} \nTask description: \n  {values[2]}\n""")
                 time.sleep(0.7)
-            else:
-                print(f"No task for {username_input} at the moment!")
-                break
+                
     # condition to exit the program            
     elif menu == 'e':
         print('\nGoodbye!!!')
